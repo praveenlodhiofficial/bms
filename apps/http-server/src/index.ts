4d4bdca76fcecd4eef152ce82194f1de6a220823
Praveen Lodhi <praveenlodhi.official@gmail.com>
2025-08-03 03:05:34 +0530

    completed the app

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -24,6 +24,6 @@ app.post("/signup", async (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(3002, () => {
+  console.log("Server is running on port 3002");
 });
